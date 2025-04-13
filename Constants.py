fdbab1d1dac8ce2b7ca58bec903ce9ca163e81cd
zyn <96938079+zyn1014@users.noreply.github.com>
2025-04-13 22:54:14 +0800

    Add files via upload
    Remove most of the redundancy (possibly removing the whole file)

---
+++ Constants.py
@@ -0,0 +1,12 @@
+# This file stores game-wide constants
+# Will be removed once done optimizing code (TO DO)
+
+# --- Colors ---
+COLOR_WHITE = (255, 255, 255)
+COLOR_RED = (255, 0, 0)
+COLOR_GREEN = (0, 255, 0)
+COLOR_BLACK = (0, 0, 0)
+
+# --- Game Settings ---
+SCREEN_WIDTH = 1080
+SCREEN_HEIGHT = 640
